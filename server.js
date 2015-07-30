355e30d96d9d968e872cb1017566e264e570bdb8
Charles Julian Knight <charles@rabidaudio.com>
2015-07-30 12:13:19 -0400

    generating comments

--- server.js
+++ server.js
@@ -13,18 +13,30 @@ future: connect github users to podio accounts
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 var app = express();
 
 app.use(bodyParser.json());
 
+
+var comment_template = _.template("[<%= pull_request.user.login %>](<%= pull_request.user.html_url %>) issued a pull request\n\n"+
+      "## [<%= pull_request.title %>](<%= pull_request.html_url %>)\n\n" +
+      "<%= pull_request.body %>\n\n"+
+      "> commits: <%= pull_request.commits %>, "+
+        "changed files: <%= pull_request.changed_files %>, "+
+        "<%= pull_request.additions %>++/<%= pull_request.deletions %>--");
+
 app.post('/github-hook', function(req,res){
+
   if(req.headers['x-github-event'] === 'ping'){
     console.log('Received ping from github');
-    console.log(req.body.hook);
     res.end("OK");
+
   }else if(req.headers['x-github-event'] === 'pull_request'){
-    console.log(req.body);
+    var comment = comment_template(req.body);
+    console.log(comment);
     res.end("OK");
+
   }else{
     res.status(400).end('This service only listends to pull requests.');
   }
