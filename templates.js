86917d831f7c687cc3fa47c3792b986e8c1cd80f
Charles Julian Knight <charles@rabidaudio.com>
2015-07-31 14:59:58 -0400

    fixed formatting issues for release

--- templates.js
+++ templates.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var Module = {};
 
-_.each(['pull_request', 'issues'], function(action){
+_.each(['pull_request', 'issues', 'release'], function(action){
   Module[action] = _.template(fs.readFileSync('templates/'+action+".dash"));
 });
 
